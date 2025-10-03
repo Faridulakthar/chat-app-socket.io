cdd4ab9ca26a6cd11750c3ff2dd87e0610b39712
faridulakthar <faridulakthar@gmail.com>
2025-10-03 16:16:49 +0530

    feat: welcome page and other components

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,14 +1,40 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StatusBar, StyleSheet, Text, View } from "react-native";
+import React, { useEffect } from "react";
+import { colors } from "@/constants/theme";
+import Animated, { FadeInDown } from "react-native-reanimated";
+import { useRouter } from "expo-router";
 
 const SplashScreen = () => {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.replace("/(auth)/welcome");
+    }, 1500);
+  }, []);
   return (
-    <View style={{ flex: 1, backgroundColor: "white" }}>
-      <Text>SplashScreen</Text>
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor={colors.neutral900} />
+      <Animated.Image
+        source={require("../assets/images/splashImage.png")}
+        entering={FadeInDown.duration(700).springify()}
+        style={styles.logo}
+        resizeMode="contain"
+      />
     </View>
   );
 };
 
 export default SplashScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: colors.neutral900,
+  },
+  logo: {
+    height: "23%",
+    aspectRatio: 1,
+  },
+});
